4aeb496331f989a7ff101e2d14417c3df67d1800
Mohsin Khan <mariamkhan33000@gmail.com>
2024-12-22 10:57:58 +0500

    Project Intial & Scrollbar

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily : {
+        'sans' : ['inter', 'sans-serif']
+      }
+    },
+  },
+
+  plugins: [tailwindScrollbar],
+}
